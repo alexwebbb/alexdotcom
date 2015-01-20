bc73e996ca960ac9551069c5d330606138fe17f8
Alex Webb <alexwebbb@yahoo.com>
2015-01-20 16:31:49 -0600

    copied App and test directories over

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def page1
+  end
+
+  def page2
+  end
+
+  def page3
+  end
+end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get page1" do
+    get :page1
+    assert_response :success
+  end
+
+  test "should get page2" do
+    get :page2
+    assert_response :success
+  end
+
+  test "should get page3" do
+    get :page3
+    assert_response :success
+  end 
+end
