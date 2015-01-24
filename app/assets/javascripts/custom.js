f74f9482b310659794d8359183876b814db3c448
Alex Webb <alexwebbb@yahoo.com>
2015-01-24 01:41:19 -0600

    wow, gallery is all done, now to add javascript flair

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  $(".p1").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g1').toggleClass('active');
+ 
+    });
+
+  $(".p2").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g2').toggleClass('active');
+ 
+    });
+
+  $(".p3").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g3').toggleClass('active');
+ 
+    });
+
+  $(".p4").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g4').toggleClass('active');
+ 
+    });
+
+  $(".p5").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g5').toggleClass('active');
+ 
+    });
+
+  $(".p6").click(function () {
+
+  	  $('.active').removeClass('active');
+ 
+	  $('.g6').toggleClass('active');
+ 
+    });
+});
